589815b9c11e29bb2350646ff6efbc15126fa6a5
alekseiKromski <aleksei.kromski@outlook.com>
2022-07-22 18:34:36 +0300

    lesson 3

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,16 @@
-
+import java.util.StringJoiner
 
 fun main(args: Array<String>){
-    println("Hi")
+    var a1: String = "213"
+    var a2: Int = 213 //32
+    var a3: Long = 1231 //64
+
+    var a4: Float = 1.24F //32
+    var a5: Double = 1.2 //64
+
+    var a6: Short = 1 //16
+    var a7: Byte = 1 //8
+
+
+
 }
